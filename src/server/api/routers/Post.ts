60c443158d0a23af652ab6f21238e3ec4fa2ef4d
NTh-Hai <thehai318@gmail.com>
2023-03-23 00:00:32 +0700

    feat: adding supabase to upload image to supabase

--- src/server/api/routers/Post.ts
+++ src/server/api/routers/Post.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import {nanoid} from "nanoid";
 
 import {
   createTRPCRouter,
@@ -6,17 +7,25 @@ import {
   protectedProcedure,
 } from "~/server/api/trpc";
 
+import { supabase } from "lib/supabaseClient";
+
 export const PostRouter = createTRPCRouter({
   create: protectedProcedure
   .input(z.object({content: z.string(), image: z.string()}))
-  .mutation(({input, ctx}) => {
+  .mutation(async ({input, ctx}) => {
+
+    const {data, error} = await supabase.storage.from("instagram-mimic").upload(`public/${ctx.session.user.id}/${nanoid(10)}`, input.image)
+
+    if (error) { throw new Error("upload image failed")}
+
+
     return ctx.prisma.post.create({
       data: {
         name: ctx.session.user.name || "anon",
         userId: ctx.session.user.id,
         userImg: ctx.session.user.image || "/default-avatar.webp",
         content: input.content,
-        image: "",
+        image: data.path,
       }
     })
   }),
